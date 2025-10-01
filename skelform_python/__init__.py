c9902368456630b44a5e60420f0c01a8dc4c9cee
Retropaint <darkglasses1122@gmail.com>
2025-10-01 11:43:12 +0700

    reworded prop to bone

--- skelform_python/__init__.py
+++ skelform_python/__init__.py
@@ -2,6 +2,7 @@ import math
 import copy
 import zipfile
 
+
 def get_frame_by_time(armature, anim_idx, elapsed, reverse):
     anim = armature["animations"][anim_idx]
     last_frame = anim["keyframes"][-1]["frame"]
@@ -16,49 +17,49 @@ def get_frame_by_time(armature, anim_idx, elapsed, reverse):
 
 
 def animate(armature, anim_idx, frame, after_animate=None):
-    props = []
+    bones = []
     keyframes = armature["animations"][anim_idx]["keyframes"]
 
     frame %= keyframes[-1]["frame"]
 
     for bone in armature["bones"]:
-        prop = copy.deepcopy(bone)
-        props.append(prop)
+        bone = copy.deepcopy(bone)
+        bones.append(bone)
 
         # interpolate
         # yapf: disable
-        prop["rot"]        += animate_float(keyframes, frame, prop["id"], "Rotation",  0)
-        prop["pos"]["x"]   += animate_float(keyframes, frame, prop["id"], "PositionX", 0)
-        prop["pos"]["y"]   += animate_float(keyframes, frame, prop["id"], "PositionY", 0)
-        prop["scale"]["x"] *= animate_float(keyframes, frame, prop["id"], "ScaleX",    1)
-        prop["scale"]["y"] *= animate_float(keyframes, frame, prop["id"], "ScaleY",    1)
+        bone["rot"]        += animate_float(keyframes, frame, bone["id"], "Rotation",  0)
+        bone["pos"]["x"]   += animate_float(keyframes, frame, bone["id"], "PositionX", 0)
+        bone["pos"]["y"]   += animate_float(keyframes, frame, bone["id"], "PositionY", 0)
+        bone["scale"]["x"] *= animate_float(keyframes, frame, bone["id"], "ScaleX",    1)
+        bone["scale"]["y"] *= animate_float(keyframes, frame, bone["id"], "ScaleY",    1)
 
         try:
-            after_animate(props, prop)
+            after_animate(bones, bone)
         except:
             pass
 
-        if prop["parent_id"] == -1:
+        if bone["parent_id"] == -1:
             continue
 
         # inherit parent
-        parent = [prop for prop in props if prop["id"] == props[-1]["parent_id"]][0]
+        parent = [bone for bone in bones if bone["id"] == bones[-1]["parent_id"]][0]
 
-        prop["rot"] += parent["rot"]
-        prop["scale"]["x"] *= parent["scale"]["x"]
-        prop["scale"]["y"] *= parent["scale"]["y"]
-        prop["pos"]["x"] *= parent["scale"]["x"]
-        prop["pos"]["y"] *= parent["scale"]["y"]
+        bones["rot"] += parent["rot"]
+        bones["scale"]["x"] *= parent["scale"]["x"]
+        bones["scale"]["y"] *= parent["scale"]["y"]
+        bones["pos"]["x"] *= parent["scale"]["x"]
+        bones["pos"]["y"] *= parent["scale"]["y"]
 
-        x = copy.deepcopy(prop["pos"]["x"])
-        y = copy.deepcopy(prop["pos"]["y"])
-        prop["pos"]["x"] = x * math.cos(parent["rot"]) - y * math.sin(parent["rot"])
-        prop["pos"]["y"] = x * math.sin(parent["rot"]) + y * math.cos(parent["rot"])
+        x = copy.copy(bone["pos"]["x"])
+        y = copy.deepcopy(bone["pos"]["y"])
+        bone["pos"]["x"] = x * math.cos(parent["rot"]) - y * math.sin(parent["rot"])
+        bone["pos"]["y"] = x * math.sin(parent["rot"]) + y * math.cos(parent["rot"])
 
-        prop["pos"]["x"] += parent["pos"]["x"]
-        prop["pos"]["y"] += parent["pos"]["y"]
+        bone["pos"]["x"] += parent["pos"]["x"]
+        bone["pos"]["y"] += parent["pos"]["y"]
 
-    return props
+    return bones
 
 
 def animate_float(keyframes, frame, bone_id, element, default):

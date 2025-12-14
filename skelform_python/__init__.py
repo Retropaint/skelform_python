ee0cda5278ae55127fc560ee047dbd46c4dd4601
Retropaint <darkglasses1122@gmail.com>
2025-12-15 01:02:37 +0700

    updated runtime for 3rd iteration

--- skelform_python/__init__.py
+++ skelform_python/__init__.py
@@ -35,28 +35,28 @@ class Bone:
     id: int
     parent_id: int
     style_ids: Optional[list[int]]
-    tex_idx: Optional[int]
+    tex: Optional[str]
     rot: float
     scale: Vec2
     pos: Vec2
+    ik_bone_ids: Optional[list[int]]
+    ik_mode: Optional[int]
+    ik_constraint_str: Optional[str]
+    ik_constraint: Optional[int]
+    ik_family_id: Optional[int]
+    ik_target_id: Optional[int]
     init_rot: float
     init_scale: Vec2
     init_pos: Vec2
+    init_ik_constraint: Optional[int]
     zindex: Optional[int] = 0
-
-
-@dataclass
-class IkFamily:
-    target_id: int
-    constraint: str
-    bone_ids: list[int]
 
 
 @dataclass
 class Keyframe:
     frame: int
     bone_id: int
-    element: str
+    element: int
     value: float
 
 
@@ -72,6 +72,7 @@ class Texture:
     name: str
     offset: Vec2
     size: Vec2
+    atlas_idx: int
 
 
 @dataclass
@@ -81,31 +82,39 @@ class Style:
 
 
 @dataclass
+class Atlas:
+    filename: str
+    size: Vec2
+
+
+@dataclass
 class Armature:
     bones: list[Bone]
+    ik_root_ids: list[int]
     animations: Optional[list[Animation]]
-    ik_families: Optional[list[IkFamily]]
+    atlases: list[Atlas]
     styles: list[Style]
 
 
-def animate(armature, animation: Animation, frame, blend_frames):
+def animate(
+    armature: Armature, animations: [Animation], frames: [int], blend_frames: [int]
+):
     bones = []
-    kf = animation.keyframes
-    bf = blend_frames
-    ikf = interpolate_keyframes
+    for a in range(len(animations)):
+        kf = animations[a].keyframes
+        bf = blend_frames[a]
+        ikf = interpolate_keyframes
 
-    for bone in armature.bones:
-        bone = copy.deepcopy(bone)
-        bones.append(bone)
-        id = bone.id
-
-        # interpolate
-        # yapf: disable
-        bone.rot     = ikf("Rotation",  bone.rot,     bone.init_rot,     kf, frame, id, bf)
-        bone.pos.x   = ikf("PositionX", bone.pos.x,   bone.init_pos.x,   kf, frame, id, bf)
-        bone.pos.y   = ikf("PositionY", bone.pos.y,   bone.init_pos.y,   kf, frame, id, bf)
-        bone.scale.x = ikf("ScaleX",    bone.scale.x, bone.init_scale.x, kf, frame, id, bf)
-        bone.scale.y = ikf("ScaleY",    bone.scale.y, bone.init_scale.y, kf, frame, id, bf)
+        for bone in armature.bones:
+            bone = copy.deepcopy(bone)
+            bones.append(bone)
+            id = bone.id
+            # yapf: disable
+            bone.pos.x   = ikf(0, bone.pos.x,   bone.init_pos.x,   kf, frames[a], id, bf)
+            bone.pos.y   = ikf(1, bone.pos.y,   bone.init_pos.y,   kf, frames[a], id, bf)
+            bone.rot     = ikf(2, bone.rot,     bone.init_rot,     kf, frames[a], id, bf)
+            bone.scale.x = ikf(3, bone.scale.x, bone.init_scale.x, kf, frames[a], id, bf)
+            bone.scale.y = ikf(4, bone.scale.y, bone.init_scale.y, kf, frames[a], id, bf)
 
     return bones
 
@@ -148,81 +157,128 @@ def normalize(vec):
     return Vec2(vec.x / mag, vec.y / mag)
 
 
-def inverse_kinematics(bones, ik_families):
+def construct(armature: Armature):
+    inh_props = copy.deepcopy(armature.bones)
+
+    inh_props = inheritance(inh_props, {})
+    ik_rots = inverse_kinematics(inh_props, armature.ik_root_ids)
+
+    final_bones = copy.deepcopy(armature.bones)
+    final_bones = inheritance(final_bones, ik_rots)
+
+    return final_bones
+
+
+def inverse_kinematics(bones: list[Bone], ik_root_ids: list[int]):
     ik_rots = {}
 
-    for family in ik_families:
-        if family.target_id == -1:
+    for root_id in ik_root_ids:
+        family = bones[root_id]
+        if (
+            family.ik_target_id == -1
+            or not family.ik_bone_ids
+            or not family.ik_target_id
+        ):
             continue
 
-        root = copy.deepcopy(bones[family.bone_ids[0]].pos)
-        target = copy.deepcopy(bones[family.target_id].pos)
+        root = copy.deepcopy(bones[family.ik_bone_ids[0]].pos)
+        target = copy.deepcopy(bones[family.ik_target_id].pos)
 
         for i in range(10):
             fabrik(family, bones, root, target)
 
         # setting bone rotations
-        end_bone = bones[family.bone_ids[-1]].pos
+        end_bone = bones[family.ik_bone_ids[-1]].pos
         tip_pos = end_bone
-        for i in range(len(family.bone_ids) - 1, -1, -1):
-            dir = tip_pos - bones[family.bone_ids[i]].pos
-            tip_pos = bones[family.bone_ids[i]].pos
-            bones[family.bone_ids[i]].rot = math.atan2(dir.y, dir.x)
+        for i in range(len(family.ik_bone_ids) - 1, -1, -1):
+            dir = tip_pos - bones[family.ik_bone_ids[i]].pos
+            tip_pos = bones[family.ik_bone_ids[i]].pos
+            bones[family.ik_bone_ids[i]].rot = math.atan2(dir.y, dir.x)
 
         # applying constraint
-        joint_dir = normalize(bones[family.bone_ids[1]].pos - root)
+        joint_dir = normalize(bones[family.ik_bone_ids[1]].pos - root)
         base_dir = normalize(target - root)
         dir = joint_dir.x * base_dir.y - base_dir.x * joint_dir.y
         base_angle = math.atan2(base_dir.y, base_dir.x)
-        cw = family.constraint == "Clockwise" and dir > 0
-        ccw = family.constraint == "CounterClockwise" and dir < 0
+        cw = family.ik_constraint == 1 and dir > 0
+        ccw = family.ik_constraint == 2 and dir < 0
         if ccw or cw:
-            for i in family.bone_ids:
+            for i in family.ik_bone_ids:
                 bones[i].rot = -bones[i].rot + base_angle * 2
 
         # saving rotations to map
-        for i in range(len(family.bone_ids) - 1):
-            ik_rots[family.bone_ids[i]] = bones[family.bone_ids[i]].rot
+        for i in range(len(family.ik_bone_ids) - 1):
+            ik_rots[family.ik_bone_ids[i]] = bones[family.ik_bone_ids[i]].rot
 
     return ik_rots
 
 
 def fabrik(family, bones, root, target):
     # forward reaching
-    next_pos = bones[family.target_id].pos
+    next_pos = bones[family.ik_target_id].pos
     next_length = 0
-    for i in range(len(family.bone_ids) - 1, -1, -1):
+    for i in range(len(family.ik_bone_ids) - 1, -1, -1):
         length = Vec2(0, 0)
-        if i != len(family.bone_ids) - 1:
-            length = normalize(next_pos - bones[family.bone_ids[i]].pos)
+        if i != len(family.ik_bone_ids) - 1:
+            length = normalize(next_pos - bones[family.ik_bone_ids[i]].pos)
             length.x *= next_length
             length.y *= next_length
 
         if i != 0:
-            next_bone = bones[family.bone_ids[i - 1]]
-            bone_pos = bones[family.bone_ids[i]].pos
+            next_bone = bones[family.ik_bone_ids[i - 1]]
+            bone_pos = bones[family.ik_bone_ids[i]].pos
             next_length = magnitude(bone_pos - next_bone.pos)
 
-        bones[family.bone_ids[i]].pos = next_pos - length
-        next_pos = bones[family.bone_ids[i]].pos
+        bones[family.ik_bone_ids[i]].pos = next_pos - length
+        next_pos = bones[family.ik_bone_ids[i]].pos
 
     # backward reaching
     prev_pos = root
     prev_length = 0
-    for i in range(len(family.bone_ids)):
+    for i in range(len(family.ik_bone_ids)):
         length = Vec2(0, 0)
         if i != 0:
-            length = normalize(prev_pos - bones[family.bone_ids[i]].pos)
+            length = normalize(prev_pos - bones[family.ik_bone_ids[i]].pos)
             length.x *= prev_length
             length.y *= prev_length
 
-        if i != len(family.bone_ids) - 1:
-            prev_bone = bones[family.bone_ids[i + 1]]
-            bone_pos = bones[family.bone_ids[i]].pos
+        if i != len(family.ik_bone_ids) - 1:
+            prev_bone = bones[family.ik_bone_ids[i + 1]]
+            bone_pos = bones[family.ik_bone_ids[i]].pos
             prev_length = magnitude(bone_pos - prev_bone.pos)
 
-        bones[family.bone_ids[i]].pos = prev_pos - length
-        prev_pos = bones[family.bone_ids[i]].pos
+        bones[family.ik_bone_ids[i]].pos = prev_pos - length
+        prev_pos = bones[family.ik_bone_ids[i]].pos
+
+
+# Flips bone's rotation if either axis of provided scale is negative.
+# Returns new bone rotations
+def check_bone_flip(bone_rot: float, scale: Vec2):
+    either = scale.x < 0 or scale.y < 0
+    both = scale.x < 0 and scale.y < 0
+    if either and not both:
+        bone_rot = -bone_rot
+    return bone_rot
+
+
+# Returns a (bone.id, Texture) map of textures to draw bones with.
+def setup_bone_textures(bones: [Bone], styles: [Style]):
+    final_textures = {}
+    for bone in bones:
+        for style in styles:
+            if bone.tex is None:
+                continue
+            final_tex = {}
+            has_final = False
+            for tex in style.textures:
+                if tex.name == bone.tex:
+                    final_tex = tex
+                    has_final = True
+                    break
+            if has_final:
+                final_textures[bone.id] = final_tex
+
+    return final_textures
 
 
 def interpolate_keyframes(
